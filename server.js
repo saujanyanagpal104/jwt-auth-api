ca29f803e19ff28c09d7716a3c67db1c35629035
saujanya_nagpal <saujanyanagpal104@gmail.com>
2020-04-15 20:33:14 +0530

    auth and user controller

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const mongoose = require('mongoose');
 const dotEnv = require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000;
+
+const userController = require('./controllers/userController');
+const authController = require('./controllers/authController');
 
 mongoose.connect(`mongodb://${process.env.USER}:${process.env.PASS}@ds139342.mlab.com:39342/jwt-auth-node-api`, {useNewUrlParser: true, useUnifiedTopology: true} , () => {
     console.log('Database Connected!!')
-})
+});
+
+app.use('/users', userController);
+app.use('/api/auth', AuthController);
 
 
 app.listen(port, () => {
